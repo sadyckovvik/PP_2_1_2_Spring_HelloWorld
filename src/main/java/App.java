fe62ea9e1667661d07577405c340c5d7fac66fa2
sadyckovvik <sadyckov.viktor@yandex.ru>
2025-01-14 18:34:31 +0300

    one commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,18 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        try (ConfigurableApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class)) {
+            HelloWorld bean =
+                    (HelloWorld) applicationContext.getBean("helloworld");
+            System.out.println(bean.getMessage());
+            HelloWorld secondBean = applicationContext.getBean("helloworld", HelloWorld.class);
+            Cat firstCatBean = applicationContext.getBean("cat", Cat.class);
+            Cat secondCatBean = applicationContext.getBean("cat", Cat.class);
+            System.out.println(bean == secondBean);
+            System.out.println(firstCatBean == secondCatBean);
+        }
     }
 }
